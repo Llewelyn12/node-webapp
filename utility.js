bfd587cc0406045fb0b3fb983d0664baa69ec3de
Llewely12 <teeraphatm63@nu.ac.th>
2022-08-29 17:58:09 +0700

    codelab 6

--- utility.js
+++ utility.js
@@ -2,7 +2,7 @@
 
 const request = require("request-promise");
 const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
-const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU4ODI5Mzc3LCJleHAiOjE2NTg4MzI5NzcsIm5iZiI6MTY1ODgyOTM3NywianRpIjoick1wRmdpS2VWcDVwYTh4bCIsInN1YiI6OCwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.YYYGe1LlmUaAjiHKCrn82TnjkBPRWC6MzgTU51ouyGE"; // put access token
+const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU5NzEwODY5LCJleHAiOjE2NTk3MTQ0NjksIm5iZiI6MTY1OTcxMDg2OSwianRpIjoidkdON1BReGNnM1k5dGxvMiIsInN1YiI6OCwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.NInJXt7Gby8EW_aW70KEtn_xQEt8b7Z2H6CTMURIuZU"; // put access token
 const student = {
   student_id:633632907,
   name: 'teeraphat muaksang', // replace with your full name.
@@ -18,7 +18,7 @@ exports.findStudentbyId = function (student_id, cb) {
             method: "GET",
             uri: EXTERNAL_API+student_id,
             headers: {
-                Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU4ODM0OTA3LCJleHAiOjE2NTg4Mzg1MDcsIm5iZiI6MTY1ODgzNDkwNywianRpIjoiMENId1drTndaVVlUbHljOSIsInN1YiI6OCwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.r91q3KtSfCEXsbKvrOACnLoUtrGr16otvztsxtjVryo`
+                Authorization: `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjYxNzY5NzQ3LCJleHAiOjE2NjE3NzMzNDcsIm5iZiI6MTY2MTc2OTc0NywianRpIjoiTHJ1Q2hRYnhPZlRBVkc3WSIsInN1YiI6OCwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.iIZQ2TGPlZTuxfYJhlSCihOTT9zerVztgiXqkEapHf4`
             }
         }).then((response) => {
             console.log('Sent');
